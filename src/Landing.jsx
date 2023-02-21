9fa21f717d01f86b76542d2d0cdab047237faa72
lucascolm <lucaspaganelli20@gmail.com>
2023-02-21 13:25:53 -0300

    disabled false

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -121,7 +121,7 @@ const Landing = ({ usuarios }) => {
             </span>
           </span>
           <button
-            disabled={true}
+            
             className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
             onClick={showAlert}
           >
